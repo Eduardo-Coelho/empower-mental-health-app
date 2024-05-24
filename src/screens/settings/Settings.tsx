419f0590e3656e50985d9e18ae7b6205c5fc8d9f
Eduardo Coelho <eduardo-daniel-coelho@outlook.com>
2024-05-24 13:06:13 +0100

    setup navigation

---
+++ src/screens/settings/Settings.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export const Settings = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings Screen</Text>
+    </View>
+  );
+};
