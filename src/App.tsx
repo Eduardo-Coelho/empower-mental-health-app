d8a57ad0c70966e9c3baf0e4645d7913d3577de5
Eduardo Coelho <eduardo-daniel-coelho@outlook.com>
2024-05-24 13:05:51 +0100

    setup navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Appointments, Bookmarks, Home, Settings } from './screens';
+
+type RootStackParamList = {
+  Home: undefined;
+  Appointments: undefined;
+  Bookmarks: undefined;
+  Settings: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-  const [clicks, setclicks] = useState<number>(0);
-
   return (
-    <View style={styles.container}>
-      <Text> Count: {clicks} </Text>
-      <Button title={'Click Me!!'} onPress={() => setclicks(clicks + 1)} />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Appointments" component={Appointments} />
+        <Stack.Screen name="Bookmarks" component={Bookmarks} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ src/screens/index.ts
@@ -0,0 +1,4 @@
+export * from './home/Home';
+export * from './appointments/Appointments';
+export * from './bookmarks/Bookmarks';
+export * from './settings/Settings';
