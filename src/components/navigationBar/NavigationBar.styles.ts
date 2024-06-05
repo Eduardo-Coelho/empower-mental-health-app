af43cdc853e52a766353cd1afa4f3780b82a419e
Eduardo Coelho <eduardo-daniel-coelho@outlook.com>
2024-06-05 15:33:57 +0100

    refactoring

--- src/components/navigationBar/NavigationBar.styles.ts
+++ src/components/navigationBar/NavigationBar.styles.ts
@@ -4,13 +4,6 @@ export const navigationStyles = StyleSheet.create({
   container: {
     display: 'flex',
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    opacity: 9,
-  },
-
-  item: {
-    marginLeft: 50,
-    marginRight: 50,
+    justifyContent: 'space-around',
   },
 });
--- src/components/navigationBar/NavigationBar.tsx
+++ src/components/navigationBar/NavigationBar.tsx
@@ -1,7 +1,7 @@
 import { TouchableOpacity, View } from 'react-native';
 import IconFoundation from 'react-native-vector-icons/Foundation';
 import { navigationStyles } from './NavigationBar.styles';
-import { color } from '../../styles';
+import { colors } from '../../styles';
 
 interface NavigationBarProps {
   navigation: any;
@@ -10,23 +10,21 @@ interface NavigationBarProps {
 export const NavigationBar = ({ navigation }: NavigationBarProps) => {
   return (
     <View style={navigationStyles.container}>
-      <View style={navigationStyles.item}>
+      <View>
         <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-          <IconFoundation name="home" size={30} color={color.ghostWhite} />
+          <IconFoundation name="home" size={30} color={colors.ghostWhite} />
         </TouchableOpacity>
       </View>
-      <View style={navigationStyles.item}>
+      <View>
         <TouchableOpacity onPress={() => navigation.navigate('Appointments')}>
-          <IconFoundation name="clock" size={30} color={color.ghostWhite} />
+          <IconFoundation name="clock" size={30} color={colors.ghostWhite} />
         </TouchableOpacity>
       </View>
-      <View style={navigationStyles.item}>
+      <View>
         <TouchableOpacity onPress={() => navigation.navigate('Bookmarks')}>
-          <IconFoundation name="bookmark" size={30} color={color.ghostWhite} />
+          <IconFoundation name="bookmark" size={30} color={colors.ghostWhite} />
         </TouchableOpacity>
       </View>
-
-      <View></View>
     </View>
   );
 };
